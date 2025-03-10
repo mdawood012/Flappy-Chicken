1f1d8a96fc2878ce5bebf286d24b4616a505f6b6
Maira <m.dawood0112@gmail.com>
2025-03-10 15:26:20 +0000

    replaced chicken.png

--- flappy.py
+++ flappy.py
@@ -4,16 +4,14 @@ try:
 except ImportError :
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
-import random
-
 class Spritesheet:
     def __init__(self, imgurl, width, height, columns, rows):
         self.img = simplegui.load_image(imgurl)
         self.rows = rows
         self.columns = columns
         
-        self.frame_width = width / columns
-        self.frame_height = height / rows
+        self.frame_width = width / self.columns
+        self.frame_height = height / self.rows
         self.frame_centre_x = self.frame_width / 2
         self.frame_centre_y = self.frame_height / 2
         
@@ -33,12 +31,9 @@ class Spritesheet:
             dest_centre, dest_size
         )
     def next_frame(self):
-        self.frame_index[0] += 1
-        if (self.frame_index[0]+1) % self.columns == 0:
-            self.frame_index[0] = 0
-            self.frame_index[1] += 1
-            if (self.frame_index[1]) % self.rows == 0:
-                self.frame_index[1] = 0
+        self.frame_index[0] = (self.frame_index[0] + 1) % self.columns
+        if self.frame_index[0] == 0:
+            self.frame_index[1] = (self.frame_index[1] + 1) % self.rows
 
 class Clock:
     def __init__(self):
@@ -52,14 +47,18 @@ class Clock:
 def draw(canvas):
     clock.tick()
     sheet.draw(canvas)
-    if clock.transition(2):
+    if clock.transition(10):
         sheet.next_frame()
 
-SHEET_URL = "http://www.cs.rhul.ac.uk/courses/CS1830/sprites/runnerSheet.png"
-SHEET_WIDTH = 1440
-SHEET_HEIGHT = 1480
+SHEET_URL = "https://images.freeimages.com/vhq/images/previews/5c5/flapping-chicken-sprite-sheet-102255.png?fmt=webp&h=350"
+SHEET_WIDTH = 800
+SHEET_HEIGHT = 121
 SHEET_COLUMNS = 6
-SHEET_ROWS = 5  
+SHEET_ROWS = 1
+image = simplegui.load_image("https://images.freeimages.com/vhq/images/previews/5c5/flapping-chicken-sprite-sheet-102255.png?fmt=webp&h=350")
+print(image.get_width())
+print(image.get_height())
+
 
 frame = simplegui.create_frame("Sprite", 600, 300)  
 sheet = Spritesheet(
