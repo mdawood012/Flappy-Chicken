d4d93eecb5014eaaf22d689331d9da19a5b851a7
Maira <m.dawood0112@gmail.com>
2025-03-10 14:51:07 +0000

    added chicken spritesheets

--- flappy.py
+++ flappy.py
@@ -4,20 +4,69 @@ try:
 except ImportError :
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
-def randCol ():
-    r = random.randrange (0, 256)
-    g = random.randrange (0, 256)
-    b = random.randrange (0, 256)
-    return 'rgb('+str(r)+ ','+str(g)+ ','+str(b)+ ')'
+import random
 
-# Drawing handler :
-# this function is called 60 times per second
+class Spritesheet:
+    def __init__(self, imgurl, width, height, columns, rows):
+        self.img = simplegui.load_image(imgurl)
+        self.rows = rows
+        self.columns = columns
+        
+        self.frame_width = width / columns
+        self.frame_height = height / rows
+        self.frame_centre_x = self.frame_width / 2
+        self.frame_centre_y = self.frame_height / 2
+        
+        self.frame_index = [0,0]
+        
+    def draw(self, canvas):
+        #self.next_frame()
+        source_centre = (
+            self.frame_width * self.frame_index[0] + self.frame_centre_x,
+            self.frame_height * self.frame_index[1] + self.frame_centre_y
+        )
+        source_size = (self.frame_width, self.frame_height)
+        dest_centre = (300, 150)
+        dest_size = (100, 100)
+        canvas.draw_image(
+            self.img, source_centre, source_size,
+            dest_centre, dest_size
+        )
+    def next_frame(self):
+        self.frame_index[0] += 1
+        if (self.frame_index[0]+1) % self.columns == 0:
+            self.frame_index[0] = 0
+            self.frame_index[1] += 1
+            if (self.frame_index[1]) % self.rows == 0:
+                self.frame_index[1] = 0
+
+class Clock:
+    def __init__(self):
+        self.time = 0
+    def tick(self):
+        self.time += 1
+    def transition(self, frame_duration):
+        if frame_duration == self.time:
+            self.time = 0
+            return True
 def draw(canvas):
-    frame.set_canvas_background(randCol())
+    clock.tick()
+    sheet.draw(canvas)
+    if clock.transition(2):
+        sheet.next_frame()
 
-# Create a frame and assign the callback to the event handler
-frame = simplegui.create_frame(" Colours ", 400 , 200)
+SHEET_URL = "http://www.cs.rhul.ac.uk/courses/CS1830/sprites/runnerSheet.png"
+SHEET_WIDTH = 1440
+SHEET_HEIGHT = 1480
+SHEET_COLUMNS = 6
+SHEET_ROWS = 5  
+
+frame = simplegui.create_frame("Sprite", 600, 300)  
+sheet = Spritesheet(
+    SHEET_URL, SHEET_WIDTH, SHEET_HEIGHT,
+    SHEET_COLUMNS, SHEET_ROWS
+)
+clock = Clock()
 frame.set_draw_handler(draw)
+frame.start()
 
-# Start the frame animation
-frame.start ()
