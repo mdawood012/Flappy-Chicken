c37516f457a782ba24903f69d3f78458c84ad073
Huda Qureshi <znac180@cim-ts-node-02.cc.rhul.local>
2025-03-13 21:00:28 +0000

    rough notes

--- Welcome.py
+++ Welcome.py
@@ -1,3 +1,15 @@
 #Huda
 #work on the welcome/game over page here 
 #test here and when done add to flappy_chicken main file. 
+
+#Button properties for welcome screen
+button_x = 290
+button_y = 335
+button_width = 100
+button_height = 40
+
+#Game state variable
+screen = "welcome"
+#if statment to be used '= welcome' then use current background settings implementation
+#if '=game' then still background with title and start button
+#if '=end' then still background  with score? and a restart button 
