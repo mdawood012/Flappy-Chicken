365c35fe888e609c7c7d00b019c64ebd8b1a0aac
Maira <m.dawood0112@gmail.com>
2025-03-16 12:59:25 +0000

    correct

--- Vector.py
+++ Vector.py
@@ -47,91 +47,91 @@ class Vector:
         return f'Vector({self.x}, {self.y})'
 
 
-    # # Returns a string representation of the vector
-    # def __str__(self):
-    #     return "(" + str(self.x) + "," + str(self.y) + ")"
-    #
-    # # Tests the equality of this vector and another
-    # def __eq__(self, other):
-    #     return self.x == other.x and self.y == other.y
-    #
-    # # Tests the inequality of this vector and another
-    # def __ne__(self, other):
-    #     return not self.__eq__(other)
-    #
-    # def __add__(self, other):
-    #     return self.copy().add(other)
-    #
-    # # Negates the vector (makes it point in the opposite direction)
-    # def negate(self):
-    #     return self.multiply(-1)
-    #
-    # def __neg__(self):
-    #     return self.copy().negate()
-    #
-    # # Subtracts another vector from this vector
-    # def subtract(self, other):
-    #     return self.add(-other)
-    #
-    # def __sub__(self, other):
-    #     return self.copy().subtract(other)
-    #
-    # # Multiplies the vector by a scalar
-    #
-    # def __mul__(self, k):
-    #     return self.copy().multiply(k)
-    #
-    # def __rmul__(self, k):
-    #     return self.copy().multiply(k)
-    #
-    # # Divides the vector by a scalar
-    # def divide(self, k):
-    #     return self.multiply(1 / k)
-    #
-    # def __truediv__(self, k):
-    #     return self.copy().divide(k)
-    #
-    # # Returns a normalized version of the vector
-    # def get_normalized(self):
-    #     return self.copy().normalize()
-    #
-    # # Returns the dot product of this vector with another one
-    # def dot(self, other):
-    #     return self.x * other.x + self.y * other.y
-    #
-    # # Returns the squared length of the vector
-    # def length_squared(self):
-    #     return self.x ** 2 + self.y ** 2
-    #
-    # # Reflect this vector on a normal
-    # def reflect(self, normal):
-    #     n = normal.copy()
-    #     n.multiply(2 * self.dot(normal))
-    #     self.subtract(n)
-    #     return self
-    #
-    # # Returns the angle between this vector and another one
-    # def angle(self, other):
-    #     return math.acos(self.dot(other) / (self.length() * other.length()))
-    #
-    # # Rotates the vector 90 degrees anticlockwise
-    # def rotate_anti(self):
-    #     self.x, self.y = -self.y, self.x
-    #     return self
-    #
-    # # Rotates the vector according to an angle theta given in radians
-    # def rotate_rad(self, theta):
-    #     rx = self.x * math.cos(theta) - self.y * math.sin(theta)
-    #     ry = self.x * math.sin(theta) + self.y * math.cos(theta)
-    #     self.x, self.y = rx, ry
-    #     return self
-    #
-    # # Rotates the vector according to an angle theta given in degrees
-    # def rotate(self, theta):
-    #     theta_rad = theta / 180 * math.pi
-    #     return self.rotate_rad(theta_rad)
-    #
-    # # project the vector onto a given vector
-    # def get_proj(self, vec):
-    #     unit = vec.get_normalized()
-    #     return unit.multiply(self.dot(unit))
+    # Returns a string representation of the vector
+    def __str__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+    
+    # Tests the equality of this vector and another
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    # Tests the inequality of this vector and another
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+    def __add__(self, other):
+        return self.copy().add(other)
+    
+    # Negates the vector (makes it point in the opposite direction)
+    def negate(self):
+        return self.multiply(-1)
+    
+    def __neg__(self):
+        return self.copy().negate()
+    
+    # Subtracts another vector from this vector
+    def subtract(self, other):
+        return self.add(-other)
+    
+    def __sub__(self, other):
+        return self.copy().subtract(other)
+    
+    # Multiplies the vector by a scalar
+    
+    def __mul__(self, k):
+        return self.copy().multiply(k)
+    
+    def __rmul__(self, k):
+        return self.copy().multiply(k)
+    
+    # Divides the vector by a scalar
+    def divide(self, k):
+        return self.multiply(1 / k)
+    
+    def __truediv__(self, k):
+        return self.copy().divide(k)
+    
+    # Returns a normalized version of the vector
+    def get_normalized(self):
+        return self.copy().normalize()
+    
+    # Returns the dot product of this vector with another one
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+    
+    # Returns the squared length of the vector
+    def length_squared(self):
+        return self.x ** 2 + self.y ** 2
+    
+    # Reflect this vector on a normal
+    def reflect(self, normal):
+        n = normal.copy()
+        n.multiply(2 * self.dot(normal))
+        self.subtract(n)
+        return self
+    
+    # Returns the angle between this vector and another one
+    def angle(self, other):
+        return math.acos(self.dot(other) / (self.length() * other.length()))
+    
+    # Rotates the vector 90 degrees anticlockwise
+    def rotate_anti(self):
+        self.x, self.y = -self.y, self.x
+        return self
+    
+    # Rotates the vector according to an angle theta given in radians
+    def rotate_rad(self, theta):
+        rx = self.x * math.cos(theta) - self.y * math.sin(theta)
+        ry = self.x * math.sin(theta) + self.y * math.cos(theta)
+        self.x, self.y = rx, ry
+        return self
+    
+    # Rotates the vector according to an angle theta given in degrees
+    def rotate(self, theta):
+        theta_rad = theta / 180 * math.pi
+        return self.rotate_rad(theta_rad)
+    
+    # project the vector onto a given vector
+    def get_proj(self, vec):
+        unit = vec.get_normalized()
+        return unit.multiply(self.dot(unit))
