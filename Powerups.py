51979d3894e52155618fceaa361bb0ce31064bfc
Maira <m.dawood0112@gmail.com>
2025-03-16 12:56:58 +0000

    mod

--- Powerups.py
+++ Powerups.py
@@ -5,14 +5,30 @@
 #what i want is that when the chicken touches any of the powerups teh score doesnt go down 
 from Vector import Vector
 
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+    
+
 class Powerups:
-    def __init__(self, pos):
+    def __init__(self, imgurl, pos, obj_width, obj_height, width, height):
+        self.img = simplegui.load_image(imgurl)
         self.pos = pos
+        self.obj_width = obj_width
+        self.obj_height = obj_height
+        self.width = width
+        self.height = height
         self.vel = Vector(0, 0.2)
     def update(self):
-        #movement 
-        pass
+        self.pos.add(self.vel)
+    def collide(self, chicken):
+        distance = self.pos.copy().subtract(chicken.pos).length()
+        return distance < self.obj_width/2 + chicken.obj_width/2     
     def draw(self, canvas):
-        #image drawn on screen
-        pass
+        center_source = (self.obj_width // 2, self.obj_height // 2)
+        center_dest = self.pos.get_p()
+        size_source = (self.obj_width, self.obj_height)
+        size_dest = (self.width/20, self.height/20)
+        canvas.draw_image(self.img, center_source, size_source, center_dest, size_dest)
     
