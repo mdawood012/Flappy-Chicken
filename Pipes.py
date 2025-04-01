0d0ea0c1e7ff9f8e775bbeb5938f66dc2543a5ba
luke1234543 <lukesummers42@gmail.com>
2025-04-01 11:53:46 +0100

    mod

--- Pipes.py
+++ Pipes.py
@@ -1,10 +1,15 @@
-
-
 import random
 from Vector import Vector
 
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
+# handles spawning a single pipe pair
 class Pipes:
+    golden_counter = 0  # golden pipe tracker
+
     def __init__(self, width, height, pipe_width, pipe_gap, speed):
         self.width = width
         self.height = height
@@ -18,9 +23,18 @@ class Pipes:
         # randomize the gap position
         self.set_random_gap()
 
-        # Pipe Colors
-        self.pipe_color = "Green"
-        self.pipe_border = "DarkGreen"
+        # Pipe images
+        self.pipe_bottom_img = simplegui._load_local_image('pipe.png')
+        self.pipe_top_img = simplegui._load_local_image('pipe_top.png')
+        self.pipe_img_width = 720
+        self.pipe_img_height = 752
+
+        # Golden pipe images
+        self.gold_pipe_bottom_img = simplegui._load_local_image('gold_pipe.png')  # golden image
+        self.gold_pipe_top_img = simplegui._load_local_image('gold_pipe_top.png')  # golden image
+
+        Pipes.golden_counter += 1
+        self.is_golden = Pipes.golden_counter % 20 == 0  # every 20th pipe
 
         # Flag to track if pipe has been passed
         self.passed = False
@@ -49,21 +63,30 @@ class Pipes:
         return self.pos.x < -self.pipe_width / 2
 
     def draw(self, canvas):
+        pipe_draw_width = self.pipe_width * 5
+        pipe_draw_height = 300
+
+        # select correct image set
+        top_img = self.gold_pipe_top_img if self.is_golden else self.pipe_top_img  # golden check
+        bottom_img = self.gold_pipe_bottom_img if self.is_golden else self.pipe_bottom_img  # golden check
+
         # draw top pipe
-        canvas.draw_polygon([
-            (self.pos.x - self.pipe_width / 2, 0),
-            (self.pos.x + self.pipe_width / 2, 0),
-            (self.pos.x + self.pipe_width / 2, self.gap_top),
-            (self.pos.x - self.pipe_width / 2, self.gap_top)
-        ], 1, self.pipe_border, self.pipe_color)
+        canvas.draw_image(
+            top_img,
+            (self.pipe_img_width / 2, self.pipe_img_height / 2),
+            (self.pipe_img_width, self.pipe_img_height),
+            (self.pos.x, self.gap_top - pipe_draw_height / 2),
+            (pipe_draw_width, pipe_draw_height)
+        )
 
         # draw bottom pipe
-        canvas.draw_polygon([
-            (self.pos.x - self.pipe_width / 2, self.gap_bottom),
-            (self.pos.x + self.pipe_width / 2, self.gap_bottom),
-            (self.pos.x + self.pipe_width / 2, self.height),
-            (self.pos.x - self.pipe_width / 2, self.height)
-        ], 1, self.pipe_border, self.pipe_color)
+        canvas.draw_image(
+            bottom_img,
+            (self.pipe_img_width / 2, self.pipe_img_height / 2),
+            (self.pipe_img_width, self.pipe_img_height),
+            (self.pos.x, self.gap_bottom + pipe_draw_height / 2),
+            (pipe_draw_width, pipe_draw_height)
+        )
 
     def check_collision(self, bird):
         # Checks if birds is withing horizontal range of the pipe
